fa955190f6929c6cd0fe339e9bdfa1800c42209b
gerviba <gerviba@sch.bme.hu>
2019-01-20 21:42:21 +0100

    Change banner

--- src/main/java/hu/gerviba/webschop/WebschopApp.java
+++ src/main/java/hu/gerviba/webschop/WebschopApp.java
@@ -8,14 +8,6 @@ public class WebschopApp {
 
     public static void main(String[] args) {
         SpringApplication.run(WebschopApp.class, args);
-        System.out.println(
-                "  _ _ _ _____ _____ _____ _____ _____ _____ _____ \n" + 
-                " | | | |   __| __  |   __|     |  |  |     |  _  |\n" + 
-                " | | | |   __| __ -|__   |   --|     |  |  |   __|\n" + 
-                " |_____|_____|_____|_____|_____|__|__|_____|__|   \n" + 
-                "  :: WEBSCHOP ::              Startup completed   \n" + 
-                "  Author: Szabo Gergely\n" +
-                "  Github: https://github.com/Gerviba/webschop    \n");
     }
     
 }
