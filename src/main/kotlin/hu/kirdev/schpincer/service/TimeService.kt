5c050b9ad9d80397f1d7a26c02e0bce4d611938c
Horváth István <horvath.isti@edu.bme.hu>
2025-05-12 16:52:18 +0200

    Fix concurrent modification exception

--- src/main/kotlin/hu/kirdev/schpincer/service/TimeService.kt
+++ src/main/kotlin/hu/kirdev/schpincer/service/TimeService.kt
@@ -6,12 +6,8 @@ import java.text.SimpleDateFormat
 @Service
 class TimeService {
 
-    private val dateTimeByFormat = mutableMapOf<String, SimpleDateFormat>()
-
     fun format(time: Any?, format: String): String? {
-        return if (time == null) null else dateTimeByFormat
-                .computeIfAbsent(format) { SimpleDateFormat(it) }
-                .format(time)
+        return if (time == null) null else SimpleDateFormat(format).format(time)
     }
 
 }
